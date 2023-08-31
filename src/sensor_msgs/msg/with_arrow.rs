2be014e1b19d4cc4c628cbcfd9d3134e3ba4d2c2
jerry73204 <jerry73204@gmail.com>
2023-08-31 19:57:48 +0800

    Add arrow Array to PointCloud2 conversion.

--- src/sensor_msgs/msg/with_arrow.rs
+++ src/sensor_msgs/msg/with_arrow.rs
@@ -1,8 +1,11 @@
 use anyhow::anyhow;
+use anyhow::bail;
 use anyhow::ensure;
 use anyhow::Result;
+use arrow::array::Array;
 use arrow::array::ArrayData;
 use arrow::array::ArrayRef;
+use arrow::array::AsArray;
 use arrow::array::FixedSizeListArray;
 use arrow::array::Float32Array;
 use arrow::array::Float64Array;
@@ -16,11 +19,14 @@ use arrow::array::UInt8Array;
 use arrow::buffer::Buffer;
 use arrow::datatypes::DataType;
 use arrow::datatypes::Field;
+use itertools::izip;
 use itertools::Itertools;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
+use num_traits::ToBytes;
 use r2r::sensor_msgs::msg::PointCloud2;
 use r2r::sensor_msgs::msg::PointField;
+use r2r::std_msgs::msg::Header;
 use std::sync::Arc;
 
 macro_rules! make_array {
@@ -82,8 +88,12 @@ macro_rules! make_list_array {
     }};
 }
 
-pub trait PointCloud2ArrowExt {
+pub trait PointCloud2ArrowExt
+where
+    Self: Sized,
+{
     fn to_arrow_array(&self) -> Result<StructArray>;
+    fn from_arrow_array(header: Header, array: &StructArray) -> Result<Self>;
 }
 
 impl PointCloud2ArrowExt for PointCloud2 {
@@ -124,10 +134,10 @@ impl PointCloud2ArrowExt for PointCloud2 {
                     count,
                 } = *field;
 
-                let datatype = PointCloud2DataType::from_u8(datatype)
+                let datatype = RosDataType::from_u8(datatype)
                     .ok_or_else(|| anyhow!("Unsupported datatype {datatype}"))?;
                 let arrow_datatype = datatype.to_arrow_datatype();
-                let size = arrow_datatype.size();
+                let size = datatype.size();
                 let field = Field::new(name, arrow_datatype, false);
 
                 Ok(FieldDesc {
@@ -163,7 +173,8 @@ impl PointCloud2ArrowExt for PointCloud2 {
                     point_bytes[start..end].chunks(data_size)
                 });
 
-                use PointCloud2DataType as T;
+                use DataType as D;
+                use RosDataType as T;
 
                 let array: ArrayRef = if count == 1 {
                     match datatype {
@@ -179,28 +190,28 @@ impl PointCloud2ArrowExt for PointCloud2 {
                 } else {
                     match datatype {
                         T::I8 => {
-                            make_list_array!(elem_iter, name, count, i8, DataType::Int8, is_be)
+                            make_list_array!(elem_iter, name, count, i8, D::Int8, is_be)
                         }
                         T::U8 => {
-                            make_list_array!(elem_iter, name, count, u8, DataType::UInt8, is_be)
+                            make_list_array!(elem_iter, name, count, u8, D::UInt8, is_be)
                         }
                         T::I16 => {
-                            make_list_array!(elem_iter, name, count, i16, DataType::Int16, is_be)
+                            make_list_array!(elem_iter, name, count, i16, D::Int16, is_be)
                         }
                         T::U16 => {
-                            make_list_array!(elem_iter, name, count, u16, DataType::UInt16, is_be)
+                            make_list_array!(elem_iter, name, count, u16, D::UInt16, is_be)
                         }
                         T::I32 => {
-                            make_list_array!(elem_iter, name, count, i32, DataType::Int32, is_be)
+                            make_list_array!(elem_iter, name, count, i32, D::Int32, is_be)
                         }
                         T::U32 => {
-                            make_list_array!(elem_iter, name, count, u32, DataType::UInt32, is_be)
+                            make_list_array!(elem_iter, name, count, u32, D::UInt32, is_be)
                         }
                         T::F32 => {
-                            make_list_array!(elem_iter, name, count, f32, DataType::Float32, is_be)
+                            make_list_array!(elem_iter, name, count, f32, D::Float32, is_be)
                         }
                         T::F64 => {
-                            make_list_array!(elem_iter, name, count, f64, DataType::Float64, is_be)
+                            make_list_array!(elem_iter, name, count, f64, D::Float64, is_be)
                         }
                     }
                 };
@@ -212,12 +223,190 @@ impl PointCloud2ArrowExt for PointCloud2 {
         let array = StructArray::from(columns);
         Ok(array)
     }
+
+    fn from_arrow_array(header: Header, array: &StructArray) -> Result<Self> {
+        let is_be = cfg!(target_endian = "big");
+        let mut offset = 0;
+
+        let fields: Vec<_> = izip!(array.column_names(), array.columns())
+            .map(|(name, col): (&str, _)| {
+                let dt = col.data_type();
+
+                let point_field = if let Some(ros_dt) = RosDataType::from_arrow_datatype(dt) {
+                    let field = PointField {
+                        name: name.to_string(),
+                        offset: offset as u32,
+                        datatype: ros_dt as u8,
+                        count: 1,
+                    };
+                    offset += dt.size();
+                    field
+                } else {
+                    let DataType::FixedSizeList(ref field_ty, count) = *dt else {
+                        bail!("Unsupported data type {dt}",);
+                    };
+                    let field_dt = field_ty.data_type();
+                    let Some(ros_dt) = RosDataType::from_arrow_datatype(field_dt) else {
+                        bail!("Unsupported data type {field_dt}",);
+                    };
+
+                    let field = PointField {
+                        name: name.to_string(),
+                        offset: offset as u32,
+                        datatype: ros_dt as u8,
+                        count: count as u32,
+                    };
+                    offset += field_dt.size() * count as usize;
+                    field
+                };
+
+                Ok(point_field)
+            })
+            .try_collect()?;
+
+        let point_step = offset;
+        let row_step = point_step;
+        let height = array.len();
+        let mut data = vec![0u8; height * row_step];
+
+        izip!(array.columns(), &fields).for_each(|(col, field)| {
+            let PointField { offset, count, .. } = *field;
+            let arrow_data_type = col.data_type();
+            let range = {
+                let start = offset as usize;
+                let end = start + arrow_data_type.size() * count as usize;
+                start..end
+            };
+
+            let point_bytes_iter = data
+                .chunks_mut(row_step)
+                .map(move |row| &mut row[range.clone()]);
+
+            macro_rules! write_column {
+                ($iter:ident, $array:ident, $is_be:ident, $array_ty:ty) => {{
+                    let array: &$array_ty = $array.as_primitive();
+
+                    if $is_be {
+                        izip!($iter, array.values()).for_each(|(bytes, val)| {
+                            bytes.copy_from_slice(&val.to_be_bytes());
+                        });
+                    } else {
+                        izip!($iter, array.values()).for_each(|(bytes, val)| {
+                            bytes.copy_from_slice(&val.to_le_bytes());
+                        });
+                    }
+                }};
+            }
+
+            macro_rules! write_list_column {
+                ($iter:ident, $array:ident, $is_be:ident, $field:ident, $array_ty:ty) => {{
+                    let array: &FixedSizeListArray = $array.as_fixed_size_list();
+                    let elem_size = $field.data_type().size();
+
+                    if $is_be {
+                        izip!($iter, 0..array.len()).for_each(|(point_bytes, idx)| {
+                            let list = array.value(idx);
+                            let list: &$array_ty = list.as_primitive();
+
+                            izip!(point_bytes.chunks_mut(elem_size), list.values()).for_each(
+                                |(elem_bytes, val)| {
+                                    elem_bytes.copy_from_slice(&val.to_be_bytes());
+                                },
+                            );
+                        });
+                    } else {
+                        izip!($iter, 0..array.len()).for_each(|(point_bytes, idx)| {
+                            let list = array.value(idx);
+                            let list: &$array_ty = list.as_primitive();
+
+                            izip!(point_bytes.chunks_mut(elem_size), list.values()).for_each(
+                                |(elem_bytes, val)| {
+                                    elem_bytes.copy_from_slice(&val.to_le_bytes());
+                                },
+                            );
+                        });
+                    }
+                }};
+            }
+
+            match arrow_data_type {
+                DataType::Int8 => {
+                    write_column!(point_bytes_iter, col, is_be, Int8Array);
+                }
+                DataType::Int16 => {
+                    write_column!(point_bytes_iter, col, is_be, Int16Array);
+                }
+                DataType::Int32 => {
+                    write_column!(point_bytes_iter, col, is_be, Int32Array);
+                }
+                DataType::UInt8 => {
+                    write_column!(point_bytes_iter, col, is_be, UInt8Array);
+                }
+                DataType::UInt16 => {
+                    write_column!(point_bytes_iter, col, is_be, UInt16Array);
+                }
+                DataType::UInt32 => {
+                    write_column!(point_bytes_iter, col, is_be, UInt32Array);
+                }
+                DataType::Float32 => {
+                    write_column!(point_bytes_iter, col, is_be, Float32Array);
+                }
+                DataType::Float64 => {
+                    write_column!(point_bytes_iter, col, is_be, Float64Array);
+                }
+                DataType::FixedSizeList(field, _) => {
+                    let field_ty = field.data_type();
+
+                    match field_ty {
+                        DataType::Int8 => {
+                            write_list_column!(point_bytes_iter, col, is_be, field, Int8Array);
+                        }
+                        DataType::Int16 => {
+                            write_list_column!(point_bytes_iter, col, is_be, field, Int16Array);
+                        }
+                        DataType::Int32 => {
+                            write_list_column!(point_bytes_iter, col, is_be, field, Int32Array);
+                        }
+                        DataType::UInt8 => {
+                            write_list_column!(point_bytes_iter, col, is_be, field, UInt8Array);
+                        }
+                        DataType::UInt16 => {
+                            write_list_column!(point_bytes_iter, col, is_be, field, UInt16Array);
+                        }
+                        DataType::UInt32 => {
+                            write_list_column!(point_bytes_iter, col, is_be, field, UInt32Array);
+                        }
+                        DataType::Float32 => {
+                            write_list_column!(point_bytes_iter, col, is_be, field, Float32Array);
+                        }
+                        DataType::Float64 => {
+                            write_list_column!(point_bytes_iter, col, is_be, field, Float64Array);
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                _ => unreachable!(),
+            }
+        });
+
+        Ok(PointCloud2 {
+            header,
+            height: height as u32,
+            width: 1,
+            fields,
+            is_bigendian: is_be,
+            point_step: point_step as u32,
+            row_step: point_step as u32,
+            data,
+            is_dense: true,
+        })
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct FieldDesc {
     field: Field,
-    datatype: PointCloud2DataType,
+    datatype: RosDataType,
     offset: usize,
     data_size: usize,
     count: usize,
@@ -225,7 +414,7 @@ struct FieldDesc {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, FromPrimitive)]
 #[repr(u8)]
-enum PointCloud2DataType {
+enum RosDataType {
     I8 = 1,
     U8 = 2,
     I16 = 3,
@@ -236,17 +425,44 @@ enum PointCloud2DataType {
     F64 = 8,
 }
 
-impl PointCloud2DataType {
+impl RosDataType {
+    pub fn size(&self) -> usize {
+        match self {
+            RosDataType::I8 => 1,
+            RosDataType::U8 => 1,
+            RosDataType::I16 => 2,
+            RosDataType::U16 => 2,
+            RosDataType::I32 => 4,
+            RosDataType::U32 => 4,
+            RosDataType::F32 => 4,
+            RosDataType::F64 => 8,
+        }
+    }
+
     pub fn to_arrow_datatype(&self) -> DataType {
         match self {
-            PointCloud2DataType::I8 => DataType::Int8,
-            PointCloud2DataType::U8 => DataType::UInt8,
-            PointCloud2DataType::I16 => DataType::Int16,
-            PointCloud2DataType::U16 => DataType::UInt8,
-            PointCloud2DataType::I32 => DataType::Int32,
-            PointCloud2DataType::U32 => DataType::UInt32,
-            PointCloud2DataType::F32 => DataType::Float32,
-            PointCloud2DataType::F64 => DataType::Float64,
+            RosDataType::I8 => DataType::Int8,
+            RosDataType::U8 => DataType::UInt8,
+            RosDataType::I16 => DataType::Int16,
+            RosDataType::U16 => DataType::UInt8,
+            RosDataType::I32 => DataType::Int32,
+            RosDataType::U32 => DataType::UInt32,
+            RosDataType::F32 => DataType::Float32,
+            RosDataType::F64 => DataType::Float64,
         }
+    }
+
+    pub fn from_arrow_datatype(dt: &DataType) -> Option<Self> {
+        Some(match dt {
+            DataType::Int8 => Self::I8,
+            DataType::UInt8 => Self::U8,
+            DataType::Int16 => Self::I16,
+            DataType::UInt16 => Self::U16,
+            DataType::Int32 => Self::I32,
+            DataType::UInt32 => Self::U32,
+            DataType::Float32 => Self::F32,
+            DataType::Float64 => Self::F64,
+            _ => return None,
+        })
     }
 }
