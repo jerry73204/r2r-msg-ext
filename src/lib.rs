ff33e596ef82ff4eccae32615ec1b13d1b519ba9
jerry73204 <jerry73204@gmail.com>
2023-09-03 15:04:38 +0800

    Add the crate-level doc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! Extensions for r2r Message Types.
+//!
+//! This crate extends common ROS message types, provided by [r2r],
+//! with type conversion to data types from third-party crates. The
+//! following crates are supported.
+//!
+//! - [nalgebra](https://docs.rs/nalgebra/)
+//! - [opencv](https://docs.rs/opencv/)
+//! - [arrow](https://docs.rs/arrow/)
+
 pub mod geometry_msgs;
 pub mod sensor_msgs;
 
